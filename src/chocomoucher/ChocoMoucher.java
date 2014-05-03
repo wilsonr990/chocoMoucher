3976b45fd422fa9d589ce8d336c8f74eb93036d6
wguevara <wguevara@easysol.net>
2014-05-03 10:44:40 -0500

    - So, here we go! this is how it starts.

---
+++ src/chocomoucher/ChocoMoucher.java
@@ -0,0 +1,21 @@
+
+package chocomoucher;
+
+import java.awt.AWTException;
+import java.awt.Dimension;
+import java.awt.HeadlessException;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+
+/**
+ *
+ * @author wilsonr
+ */
+public class ChocoMoucher{
+
+    ChocoMoucher(){
+        
+    }
+}
---
+++ src/chocomoucher/main.java
@@ -0,0 +1,13 @@
+package chocomoucher;
+
+/**
+ *
+ * @author wilsonr
+ */
+public class main {
+    public static void main(String[] args) {
+        ChocoMoucher myInterface;
+        myInterface = new ChocoMoucher();
+    }
+    
+}
