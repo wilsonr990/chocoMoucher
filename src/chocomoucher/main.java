cee5339c70b8bd0bc13e9760d5c78dc15d961e04
wilsonr990 <wilsonr990@hotmail.com>
2014-05-05 00:57:05 -0500

    - add new files and images

--- src/chocomoucher/main.java
+++ src/chocomoucher/main.java
@@ -1,13 +1,16 @@
 package chocomoucher;
 
+import java.awt.AWTException;
+
 /**
  *
  * @author wilsonr
  */
 public class main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         ChocoMoucher myInterface;
         myInterface = new ChocoMoucher();
+        myInterface.getGameInstance();
     }
     
 }
